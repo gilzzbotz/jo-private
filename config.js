4f075e3c86beaf94e65fe5c83b717a8340f02ac8
rdtyzbotz <113617871+rdtyzbotz@users.noreply.github.com>
2022-10-17 19:25:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,9 +22,9 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['6281251549899','6281251549899','6281251549899']
-global.premium = ['6281251549899']
-global.packname = 'The Jo Bot'
+global.owner = ['6285730794089','62885730794089','6285730794089']
+global.premium = ['6285730794089']
+global.packname = 'Gilzz Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
